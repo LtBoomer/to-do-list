662ce5b4f292bfac1adb49aadfcc6bd9f91fe35a
LtBoomer <ciclosan.vlad1@gmail.com>
2022-09-25 15:08:05 +0300

    final commit

--- script.js
+++ script.js
@@ -2,69 +2,70 @@ const addTask = document.querySelector("button");
 const taskInput = document.querySelector("input");
 const list = document.querySelector(".list");
 let updateButtons;
+let currentTask;
 let updateClick = false;
 let i = -1;
 let deleteButton;
 let checkButton;
 let taskList = [];
 
-const refreshTaskList = (task) => {
-  list.innerHTML += `
-  <div class="task">
-  ${task.edit ? `<input placeholder=${task.text} />` : `<p>${task.text}</p>`}
-    <div class="icons">
-      <img src="https://img.icons8.com/material-outlined/24/000000/checked--v1.png" class="checkboi"/>
-      <img src="https://img.icons8.com/pastel-glyph/24/000000/loop.png" class="update"/>
-      <img src="https://img.icons8.com/material-rounded/24/000000/trash.png" class="delete" id=${
-        task.id
-      }/>
-    </div>
-  </div>
-  `;
-  updateButton = document.querySelector(".update");
-  deleteButtons = document.querySelectorAll(".delete");
-  checkButton = document.querySelector(".checkboi");
-
-  console.log(deleteButtons);
-
-  deleteButtons.forEach((element) => {
-    element.addEventListener("click", () => {
-      taskList = taskList.filter(
-        (task) => task.id !== +element.getAttribute("id")[0]
-      );
+const keyPress = (event, id) => {
+  const inputUpdate = document.querySelector(".task-edit");
+  taskList.forEach((task) => {
+    if (task.id === +id && event.key === "Enter") {
+      task.edit = false;
+      task.text = inputUpdate.value;
       list.innerHTML = " ";
-      taskList.forEach((item) => {
-        refreshTaskList(item);
-      });
-    });
+      taskFunction(taskList, "update");
+    }
   });
-
-  updateButton.addEventListener("click", () => {});
-
-  checkButton.addEventListener("click", () => {});
 };
 
-const addTaskFunction = (task) => {
-  console.log("task>>>>", task);
-  taskList = [...taskList, task];
-  console.log(taskList);
-
+const taskCard = (task) => {
   list.innerHTML += `
-  <div class="task">
-    ${task.edit ? `<input placeholder=${task.text} />` : `<p>${task.text}</p>`}
+  <div class="task ${task.check ? "green" : ""}">
+    ${
+      task.edit
+        ? `<input onkeypress="keyPress(event, ${task.id})" placeholder=${task.text} class="task-edit" />`
+        : `<p>${task.text}</p>`
+    }
     <div class="icons">
-      <img src="https://img.icons8.com/material-outlined/24/000000/checked--v1.png" class="checkboi"/>
-      <img src="https://img.icons8.com/pastel-glyph/24/000000/loop.png" class="update"/>
+      <img src="https://img.icons8.com/material-outlined/24/000000/checked--v1.png" class="checkboi" id=${
+        task.id
+      } />
+      <img src="https://img.icons8.com/pastel-glyph/24/000000/loop.png" class="update" id=${
+        task.id
+      } />
       <img src="https://img.icons8.com/material-rounded/24/000000/trash.png" class="delete" id=${
         task.id
       } />
     </div>
   </div>
   `;
+};
+
+const taskFunction = (task, action) => {
+  console.log("task>>>>", task);
+  switch (action) {
+    case "add":
+      taskList = [...taskList, task];
+      taskCard(task);
+      break;
+    case "delete":
+    case "update":
+    case "check":
+      task.forEach((item) => {
+        taskCard(item);
+      });
+      break;
+    default:
+      break;
+  }
 
   updateButtons = document.querySelectorAll(".update");
   deleteButtons = document.querySelectorAll(".delete");
-  checkButton = document.querySelector(".checkboi");
+  checkButton = document.querySelectorAll(".checkboi");
+  currentTask = document.querySelectorAll(".task");
 
   deleteButtons.forEach((element) => {
     element.addEventListener("click", () => {
@@ -72,9 +73,7 @@ const addTaskFunction = (task) => {
         (task) => task.id !== +element.getAttribute("id")
       );
       list.innerHTML = " ";
-      taskList.forEach((item) => {
-        refreshTaskList(item);
-      });
+      taskFunction(taskList, "delete");
     });
   });
 
@@ -82,33 +81,39 @@ const addTaskFunction = (task) => {
     element.addEventListener("click", () => {
       taskList.forEach((item) => {
         if (item.id === +element.getAttribute("id")) {
-          console.log(item.id)
-          // taskList[+element.getAttribute("id")].edit = true;
-          // list.innerHTML = " ";
-          // taskList.forEach((item) => {
-          //   refreshTaskList(item);
-          // });
+          item.edit = true;
         }
-      
       });
-      // if(+element.getAttribute("id") === index){
-      //   console.log("da")
-      //   console.log(element)
-      // }
+      list.innerHTML = " ";
+      taskFunction(taskList, "update");
     });
   });
 
-  checkButton.addEventListener("click", () => {});
+  checkButton.forEach((item) => {
+    item.addEventListener("click", () => {
+      taskList.forEach((task) => {
+        if (task.id === +item.getAttribute("id")) {
+          task.check = true;
+          list.innerHTML = " ";
+          taskFunction(taskList, "check");
+        }
+      });
+    });
+  });
 };
 
 addTask.addEventListener("click", () => {
   if (taskInput.value !== "") {
     i++;
-    addTaskFunction({
-      id: i,
-      text: taskInput.value,
-      check: false,
-      edit: false,
-    });
+    taskFunction(
+      {
+        id: i,
+        text: taskInput.value,
+        check: false,
+        edit: false,
+      },
+      "add"
+    );
   }
+  taskInput.value = "";
 });
